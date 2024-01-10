2a8322645634dc659b701c9d562436c5060c0b53
Abelardo Cuesta <j.cuestaa@uniandes.edu.co>
2024-01-10 11:24:35 -0500

    API methods create getById getAll

---
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -0,0 +1,38 @@
+package net.javaguides.springboot.controller;
+
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import net.javaguides.springboot.entity.User;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+    private UserService userService;
+
+
+    @PostMapping
+    public ResponseEntity <User> createUser(@RequestBody User user){
+        User savedUser=userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId){
+        User user =userService.getUserById(userId);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users,HttpStatus.OK);
+    }
+}
